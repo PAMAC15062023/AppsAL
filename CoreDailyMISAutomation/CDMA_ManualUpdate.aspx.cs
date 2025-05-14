969f5d08641e123d850cfd95963901dab90e3ee7
Yasir Siddiqui <yasir.siddiqui@pamac.com>
2025-05-14 11:41:59 +0530

    Test2

--- CoreDailyMISAutomation/CDMA_ManualUpdate.aspx.cs
+++ CoreDailyMISAutomation/CDMA_ManualUpdate.aspx.cs
@@ -535,8 +535,6 @@ namespace CoreDailyMISAutomation
                 cmd.Parameters.AddWithValue("@UserId", Userid);
                 cmd.Parameters.AddWithValue("@Branch", Convert.ToString(Session["Branch_Name"]));
 
-                ///////Test////
-
                 sqlCon.Open();
                 int result = cmd.ExecuteNonQuery();
 
