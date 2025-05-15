c9641357841d3cb07787695620a78f8bec7f6431
Yasir Siddiqui <yasir.siddiqui@pamac.com>
2025-05-15 11:13:54 +0530

    TestYa

--- LNTFinance/LNTFinance/LoginPage.aspx.cs
+++ LNTFinance/LNTFinance/LoginPage.aspx.cs
@@ -21,6 +21,7 @@ namespace LNTFinance
                 {
                     Get_ClientList();
                     GenerateCaptcha();
+                    //TEST//
                 }
 
             }
